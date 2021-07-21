ed440f88eab8d01cc1a6197e94afc9e4a81f28f5
septa <septa.saputra@aitindo.com>
2021-07-21 11:13:48 +0700

    initialize commit

---
+++ layouts/default.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="main-wrapper">
+    <Nuxt />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    class="
+      w-full
+      min-h-screen
+      bg-indigo-600
+      flex flex-col
+      items-center
+      justify-center
+    "
+  >
+    <div class="text-4xl text-white font-bold">PBPRSI</div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
